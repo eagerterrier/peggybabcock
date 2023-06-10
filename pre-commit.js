27e434be865549ca6bc5064998c83bbd1903741d
eagerterrier <eagerterrier@gmail.com>
2023-06-10 10:32:39 +0100

    ENH: improvements to the way changes were recorded

--- pre-commit.js
+++ pre-commit.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 
+const sensibleTimes = time => time.substring(0, time.length -2);
+const removeMilliseconds = originalDate => originalDate.toISOString().split('.')[0];
+
 const dataPath = './data/';
 const templateFile = fs.readFileSync('./404.html', 'utf8');
+const templateFileModified = removeMilliseconds(new Date(fs.statSync('./404.html').mtime));
+const previousTemplateFile = fs.readFileSync('./previous-404.html', 'utf8');
+const previousTemplateFileModified = removeMilliseconds(new Date(fs.statSync('./previous-404.html').mtime));
 
 const dataFiles = fs.readdirSync(dataPath);
 let sitemapXML = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
@@ -25,12 +31,18 @@ const structuredData = {
 	"datePublished": "1997-07-31T09:26:58Z",
 }
 
+
+const shouldWriteFile = (modifiedDate, file) => {
+    return sensibleTimes(previousTemplateFileModified) !== sensibleTimes(templateFileModified) || 
+      sensibleTimes(removeMilliseconds(fs.statSync(`data/${file}`).mtime)) !== sensibleTimes(removeMilliseconds(fs.statSync(`previous-data/${file}`).mtime))
+};
+
 dataFiles.forEach((file, i) => {
   if (file.indexOf('.json') !== -1) {
       const newFileName = file.replace('.json', '.html');
-      const currentFileContents = fs.readFileSync(newFileName, 'utf8');
       let modifiedDate = new Date(fs.statSync(newFileName).mtime);
-      const data = JSON.parse(fs.readFileSync(dataPath + file, 'utf8'));
+      const dataFileContents = fs.readFileSync(dataPath + file, 'utf8');
+      const data = JSON.parse(dataFileContents);
       let toReturn = `<h2>${data.title}</h2>`;
       const newHtml = data.img ? `<img src="./img/${data.img}" alt="${data.img_alt}" />` : '';
       let newHTML = `${newHtml}<h2>${data.title}</h2>`;
@@ -45,24 +57,30 @@ dataFiles.forEach((file, i) => {
       structuredData.name = file === 'index.json' ? 'Peggy Babcock' : `${data.title} | Peggy Babcock`;
       structuredData.image = data.img ? `${baseDomain}/img/${data.img}`: `${baseDomain}/img/peggy-babcock.jpg`;
       structuredData.dateModified = modifiedDate;
+
       structuredData.headline = data.title;
       let fileContents = templateFile;
       fileContents = fileContents.replace('<title>Peggy Babcock</title>', `<title>${data.title} | Peggy Babcock</title>`);
       fileContents = fileContents.replace('___REPLACE_THIS___', newHTML);
       fileContents = fileContents.replace('<article class="visually-hidden">', '<article>');
       fileContents = fileContents.replace('<script type="application/ld+json"></script>', `<script type="application/ld+json">${JSON.stringify(structuredData)}</script>`);
-      
-      if (currentFileContents !== fileContents) {
+      if (shouldWriteFile(modifiedDate, file)) {
         console.log('changing file', newFileName);
         const previousStructuredData = structuredData;
         structuredData.dateModified = modifiedDate;
         fileContents = fileContents.replace(`<script type="application/ld+json">${JSON.stringify(previousStructuredData)}</script>`, `<script type="application/ld+json">${JSON.stringify(structuredData)}</script>`);
         fs.writeFileSync(newFileName, fileContents);
+        fs.copyFileSync(`data/${file}`, `previous-data/${file}`);
+        fs.writeFileSync(`data/${file}`, dataFileContents);
         modifiedDate = todaysDate;
       }
       sitemapXML = `${sitemapXML}<url><loc>${baseDomain}/${newFileName}</loc><lastmod>${modifiedDate.toISOString().split('T')[0]}</lastmod></url>`;
   }
 })
+
+if (previousTemplateFileModified.substring(0, previousTemplateFileModified.length -2) !== templateFileModified.substring(0, previousTemplateFileModified.length -2)) {
+     fs.writeFileSync('./previous-404.html', templateFile);
+}
 
 sitemapXML = sitemapXML + '</urlset>';
 fs.writeFileSync('sitemap.xml', sitemapXML);
